a4c2622d41b9c33134c321f761ba869229326d26
nodder3239 <show3239@naver.com>
2023-11-17 12:19:56 +0900

    JQ

--- day4/js/time.js
+++ day4/js/time.js
@@ -10,7 +10,7 @@ function myWatch(){
   let watch = document.getElementById("show");
   // watch.innerHTML = time;
   watch.style.color = "blue";
-  if(time >= 12){
+  if(time <= 12){
     watch.innerHTML = time.replace("오전", "am");
   }else{
     watch.innerHTML = time.replace("오후", "pm");
